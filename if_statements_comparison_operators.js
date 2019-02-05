ca30f5d5b6135f1b81e815052c7950f2dd91bdc3
Bilgihan Köse <bilgihankose@gmail.com>
2019-02-05 17:53:33 +0300

    Add files via upload

---
+++ if_statements_comparison_operators.js
@@ -0,0 +1,96 @@
+// if(something){
+//   do something
+// } else {
+//   do something else
+// }
+
+const id = 100;
+
+// // EQUAL TO
+// if(id == 100){ // iki tane eşittir işaret (==) bununla bunu karşılaştır demek. Tek eşittir gibi atama operatörü değil. Eğer id değeri string olarak tanımlansaydı da correct cevabı dönerdi. Çünkü string mi number mı kontrol etmiyor.
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // NOT EQUAL TO
+// if(id != 101){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // EQUAL TO VALUE & TYPE
+// if(id === 100){ // En çok tercih edileni budur. Çünkü aynı zamanda typeları da eşit mi diye kontrol ediyor.
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // EQUAL TO VALUE & TYPE
+// if(id !== 100){ 
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// Test if undefined
+// if(typeof id !== 'undefined'){ // eğer id değişkeni undefined değilse id yi console da göster, undifined sa no id yazdır.
+//   console.log(`The ID is ${id}`);
+// } else {
+//   console.log('NO ID');
+// }
+
+// GREATER OR LESS THAN
+// if(id <= 100){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// IF ELSE
+
+const color = 'yellow';
+
+// if(color === 'red'){
+//   console.log('Color is red');
+// } else if(color === 'blue'){
+//   console.log('Color is blue');
+// } else {
+//   console.log('Color is not red or blue');
+// }
+
+// LOGICAL OPERATORS
+
+const name = 'Steve';
+const age = 70;
+
+// AND &&
+if(age > 0 && age < 12){
+  console.log(`${name} is a child`);
+} else if(age >= 13 && age <= 19){
+  console.log(`${name} is a teenager`);
+} else {
+  console.log(`${name} is an adult`);
+}
+
+// OR ||
+if(age < 16 || age > 65){
+  console.log(`${name} can not run in race`);
+} else {
+  console.log(`${name} is registered for the race`);
+}
+
+// TERNARY OPERATOR // üçlü operatör.
+console.log(id === 100 ? 'CORRECT' : 'INCORRECT'); //if this is true, we want to output correct, else incorrect.
+
+// WITHOUT BRACES 
+if(id === 100)
+  console.log('CORRECT');
+else
+  console.log('INCORRECT'); //böyle kullanılabilir ama okunabilir olmadığı için önerilmiyor. 
+
+if(id === 100)
+  console.log('CORRECT');
+else
+  console.log('INCORRECT');
